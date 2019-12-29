c9eeed1f8fe65ce9afc52c651b791a2bf33997cd
cat23x <baumgart.guenter02@gmail.com>
2019-12-29 15:54:50 +0100

    1st pass ready

--- cli/build/commands/markdown.js
+++ cli/build/commands/markdown.js
@@ -9,10 +9,18 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const __1 = require("..");
+const utils = require("../lib/common/strings");
 const path = require("path");
 const read_1 = require("@xblox/fs/read");
+const exists_1 = require("@xblox/fs/exists");
+const dir_1 = require("@xblox/fs/dir");
+const write_1 = require("@xblox/fs/write");
 const showdown_1 = require("showdown");
 const fg = require('fast-glob');
+const defaultData = (override) => {
+    return Object.assign({ PART_PARENT: 'my parent', PART_INVENTORY: '', PART_NAME: 'Front Shield', PART_VERSION: 1, PART_VERSIONS: '1 2', PART_ID: 'Z_4_FRONT_SHIELD', PART_DRAWING: 'https://a360.co/37pDdVD', PART_PREVIEW_IMAGE: '', PART_COMPAT: '', PART_CAPS: '', PART_ASSEMBLY: '', PART_TOOLS: '', PART_TEMPLATES: '', PART_STOCK: '', PART_MACHINES: '', PART_STEPS: '', PART_EDIT: '' }, override);
+};
 const defaultOptions2 = (yargs) => {
     return yargs.option('input', {
         default: './',
@@ -26,28 +34,46 @@ const defaultOptions2 = (yargs) => {
     });
 };
 let options = (yargs) => defaultOptions2(yargs);
-const convert = (input) => {
+const convert = (input, data) => {
+    input = utils.replace(input, null, defaultData(data), {
+        begin: '<%',
+        end: '%>'
+    });
     let converter = new showdown_1.Converter();
     converter.setOption('literalMidWordUnderscores', 'true');
     return converter.makeHtml(input);
 };
+const convertFiles = (files, dst) => {
+    files.forEach((f) => {
+        const content = read_1.sync(f, 'string');
+        const html = convert(content, {});
+        const target = dst + '/' + path.parse(f).name + '.html';
+        write_1.sync(target, html);
+    });
+};
+// npm run build ; node ./build/main.js markdown --input=../pages --output=../out
 exports.register = (cli) => {
     return cli.command('markdown', 'Converts md files to html using showdown', options, (argv) => __awaiter(void 0, void 0, void 0, function* () {
-        const src = path.resolve('' + argv.input);
-        const dst = path.resolve('' + argv.output);
-        console.log('find in ' + src);
-        const files = fg.sync('*.md', { dot: true, cwd: src, absolute: true });
-        console.log('files ', files);
-        files.forEach((f) => {
-            const content = read_1.sync(f, 'string');
-            const html = convert(content);
-            console.log(` File : ${f} = \n   ${content} =\n ${html}`);
-        });
         if (argv.help) {
             return;
         }
-        // const args = sanitize(argv) as Options;
-        // output({}, args);
+        const src = path.resolve('' + argv.input);
+        const dst = path.resolve('' + argv.output);
+        if (!exists_1.sync(dst)) {
+            dir_1.sync(dst);
+        }
+        const files = fg.sync('*.md', { dot: true, cwd: src, absolute: true });
+        convertFiles(files, dst);
+        if (argv.debug) {
+            __1.debug(`Converted ${files.length} files`);
+        }
+        /*
+        output({
+            files : file.length
+        },{
+            format: OutputFormat.json
+        })
+        */
     }));
 };
 //# sourceMappingURL=markdown.js.map
--- cli/src/commands/markdown.ts
+++ cli/src/commands/markdown.ts
@@ -1,14 +1,38 @@
 import * as CLI from 'yargs';
-import { defaultOptions, sanitize } from '../argv';
-import { Options } from '..';
-import { render as output } from '../output';
+import { debug } from '..';
 import * as utils from '../lib/common/strings';
 import * as path from 'path';
 import { sync as read } from '@xblox/fs/read';
+import { sync as exists } from '@xblox/fs/exists';
+import { sync as dir } from '@xblox/fs/dir';
+import { sync as write } from '@xblox/fs/write';
 
 import { Converter } from 'showdown';
 
 const fg = require('fast-glob');
+
+const defaultData = (override: any) => {
+    return {
+        PART_PARENT: 'my parent',
+        PART_INVENTORY: '',
+        PART_NAME: 'Front Shield',
+        PART_VERSION: 1,
+        PART_VERSIONS: '1 2',
+        PART_ID: 'Z_4_FRONT_SHIELD',
+        PART_DRAWING: 'https://a360.co/37pDdVD',
+        PART_PREVIEW_IMAGE: '',
+        PART_COMPAT: '',
+        PART_CAPS: '',
+        PART_ASSEMBLY: '',
+        PART_TOOLS: '',
+        PART_TEMPLATES: '',
+        PART_STOCK: '',
+        PART_MACHINES: '',
+        PART_STEPS: '',
+        PART_EDIT: '',
+        ...override
+    }
+};
 
 const defaultOptions2 = (yargs: CLI.Argv) => {
     return yargs.option('input', {
@@ -25,28 +49,45 @@ const defaultOptions2 = (yargs: CLI.Argv) => {
 
 let options = (yargs: CLI.Argv) => defaultOptions2(yargs);
 
-const convert = (input: string) => {
+const convert = (input: string, data: any) => {
+    input = utils.replace(input, null, defaultData(data), {
+        begin: '<%',
+        end: '%>'
+    });
+
     let converter = new Converter();
     converter.setOption('literalMidWordUnderscores', 'true');
     return converter.makeHtml(input);
 }
 
+const convertFiles = (files,dst) =>{
+    files.forEach((f) => {
+        const content = read(f, 'string');
+        const html = convert(content as string, {});
+        const target = dst + '/' + path.parse(f).name + '.html';
+        write(target, html);
+    })
+}
+// npm run build ; node ./build/main.js markdown --input=../pages --output=../out
 export const register = (cli: CLI.Argv) => {
     return cli.command('markdown', 'Converts md files to html using showdown', options, async (argv: CLI.Arguments) => {
-
+        if (argv.help) { return; }
         const src = path.resolve('' + argv.input);
         const dst = path.resolve('' + argv.output);
-        console.log('find in ' + src);
+        if (!exists(dst)) {
+            dir(dst);
+        }
         const files = fg.sync('*.md', { dot: true, cwd: src, absolute: true });
-        console.log('files ', files);
-        files.forEach((f) => {
-            const content = read(f,'string');
-            const html = convert(content as string);
-            console.log(` File : ${f} = \n   ${content} =\n ${html}`);
+        convertFiles(files,dst);
+        if (argv.debug) {
+            debug(`Converted ${files.length} files`);
+        }
+        /*
+        output({
+            files : file.length
+        },{
+            format: OutputFormat.json
         })
-
-        if (argv.help) { return; }
-        // const args = sanitize(argv) as Options;
-        // output({}, args);
+        */
     });
 };
