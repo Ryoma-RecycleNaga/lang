a12d2b37c76cd41dc1682b59e7cd3fa395af10a2
guenter.baumgart <guenter.baumgart@dynatrace.com>
2020-01-04 01:12:06 +0100

    cleanup 1/3

--- cli/src/main.ts
+++ cli/src/main.ts
@@ -1,16 +1,7 @@
 #!/usr/bin/env node
 import { defaults } from './_cli'; defaults();
 
-import * as cli from 'yargs';
-const yargonaut = require('yargonaut').style('blue').helpStyle('green');
-
-import { info } from './log';
-import { defaultOptions, sanitize } from './argv';
-
-//import { register as registerSummary } from './commands/summary'; registerSummary(cli);
-//import { register as registerDetail } from './commands/detail'; registerDetail(cli);
-//import { register as registerClean } from './commands/clean'; registerClean(cli);
-import { register as registerMarkdown } from './commands/markdown'; registerMarkdown(cli);
+import * as cli from 'yargs';import { register as registerMarkdown } from './commands/markdown'; registerMarkdown(cli);
 import { register as registerPDF2JPG } from './commands/pdf2jpg'; registerPDF2JPG(cli);
 
 const argv = cli.argv;
