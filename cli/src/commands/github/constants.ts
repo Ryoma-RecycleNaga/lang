97eeb5d2abb2c456c2b1901c01389a2ce1f5efbb
guenter.baumgart <guenter.baumgart@dynatrace.com>
2020-04-13 00:17:47 +0200

    cli:github:product:index

--- cli/src/commands/github/constants.ts
+++ cli/src/commands/github/constants.ts
@@ -1,20 +1,10 @@
-/*
+
 // paths
-export const BAZAR_SLUG = `bazar`;
+export const GH_SLUG = `bazar`;
 export const CONFIG_FILE = 'config.json';
-export const VENDOR_CONFIG = `${BAZAR_SLUG}/${CONFIG_FILE}`;
+export const VENDOR_CONFIG = `${GH_SLUG}/${CONFIG_FILE}`;
 export const PRODUCT_CONFIG = (product_path) => `${product_path}/${CONFIG_FILE}`;
 
 // template keys
-export const PRODUCT_TEMPLATE = 'product';
-export const VENDOR_TEMPLATE = 'vendor';
+export const INDEX_TEMPLATE = 'product_index.md';
 
-// config keys
-export interface VendorConfig {
-  vendor_name?: string;
-}
-
-export interface ProductConfig {
-  vendor_name?: string;
-}
-*/
