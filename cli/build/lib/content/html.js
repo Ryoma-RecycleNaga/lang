c1df1fe291338ece50e39920624f811083d1a927
cat23x <baumgart.guenter02@gmail.com>
2020-04-19 12:26:26 +0200

    cli:thumbs

---
+++ cli/build/lib/content/html.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.img = (file) => {
+    return `<div class="thumb">
+            <img src="${file}" width="100%" />
+        </div>`;
+};
+//# sourceMappingURL=html.js.map
---
+++ cli/src/lib/content/html.ts
@@ -0,0 +1,5 @@
+export const img = (file) => {
+    return `<div class="thumb">
+            <img src="${file}" width="100%" />
+        </div>`;
+}
